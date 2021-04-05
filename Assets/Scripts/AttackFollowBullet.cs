7dda89ddecb2ea75e7f26dfd614c10981c46163d
juliasherbal <sherbaljulia@gmail.com>
2021-04-05 01:08:33 -0400

    Projectiles follow player

---
+++ Assets/Scripts/AttackFollowBullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackFollowBullet : MonoBehaviour
+{
+
+    public GameObject projectilePrefab;
+    //public float speed = 100;
+    GameObject projectile;
+
+    public float cooldown = 3;
+
+    public float cd;
+
+    bool onScreen = false;
+    void Start()
+    {
+        cd = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cd <= 0 && !gameObject.GetComponent<EnemyHealth>().enemyDead &&
+            onScreen)
+        {
+            projectile =
+                Instantiate(projectilePrefab, transform.position + transform.forward + new Vector3(0, 1.5f, 0), transform.rotation) as GameObject;
+
+            //Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            //rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+
+            projectile.transform.SetParent(GameObject.FindGameObjectWithTag("ProjectileParent").transform);
+
+            cd = cooldown;
+        }
+
+
+        if (cd > 0)
+        {
+            cd -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            onScreen = true;
+        }
+    }
+}
---
+++ Assets/Scripts/FollowProjectile.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowProjectile : MonoBehaviour
+{
+    public Transform player;
+    public float delay = 3;
+    public float speed = 3;
+    public int projectileDamage = 20;
+    public bool onScreen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, delay);
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onScreen)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.position, step);
+        }
+
+
+    }
+
+    /*private void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("I collided with the player");
+            var playerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.TakeDamage(damageAmount);
+
+        }
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            onScreen = true;
+        }
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //MAKE SURE THE ENEMIES ARE ACTUALLY TAGGED OR ELSE IT WONT DO DAMAGE!!!!!
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Deal damage to player.
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(projectileDamage);
+        }
+        else if (other.gameObject.CompareTag("MainCamera"))
+        {
+            onScreen = true;
+        }
+    }
+
+}
